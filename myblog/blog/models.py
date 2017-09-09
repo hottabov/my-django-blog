b5f84f67a2e913dd3dca381df2cabd9cfaae0052
hottabov <hottabov@gmail.com>
2017-09-09 21:53:53 +0300

    My Django Blog app, first commit

---
+++ myblog/blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=80)
+    description = models.CharField(max_length=200)
+    keywords = models.CharField(max_length=200)
+    photo = models.ImageField('Фото', upload_to='media', default='', blank=True)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
